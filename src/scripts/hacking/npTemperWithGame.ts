1a597fb4109f2ee5410bb820029319a68a8a0e71
Leto <m.staerk@gmx.de>
2022-04-09 15:24:09 +0200

    feat(tempering): Added game tempering

---
+++ src/scripts/hacking/npTemperWithGame.ts
@@ -0,0 +1,38 @@
+import {NS} from '../../@types/NetscriptDefinitions';
+
+/** @param {NS} ns **/
+export async function main(ns: NS) {
+  Math.floor = (value) => 0; // Go to the casino an bet on zero after that :D
+  Number.prototype.toExponential = () => ''; // Wait 15min for check
+  //document.location
+  // @ts-ignore
+  const doc = eval("document");
+  //doc.saveFile
+  //globalThis
+  //ns.bypass()
+  // @ts-ignore
+  doc.achievements.push('UNACHIEVABLE')
+  // @ts-ignore
+  ns.alterReality(); // Open Dev-Tools jump to the console log "I am sure that this variable is false." -> set a break point -> execute again -> change result to true ;)
+  // n00dles => backdoor the noodles bar in new tokyo i guess "." server. then go eat there
+  // @ts-ignore
+  ns.exploit();
+  // @ts-ignore
+  ns.heart.break(); // karma
+  ns.print('*******');
+
+  const unclick = doc.getElementById("unclickable");
+  const handler = Object.keys(unclick)[1];
+  unclick[handler].onClick({ target: unclick, isTrusted: true });
+
+//   if(mode === '1000threads'){
+//           this.killRunningViruses(system,[]);
+//           const maxThreads = getMaxPossibleVirusThreads(this.ns, system.host);
+//           for(let i=0;i<maxThreads;i++){
+//             if(threadCount++ < 1000){
+//               const target = hackable[i%(hackable.length-1)];
+//               ns.exec(ENPFiles.Virus, system.host, 1, '--target=' + target.host, 'jsonp='+i);
+//             }
+//           }
+//         }else
+}
